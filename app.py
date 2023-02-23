d945a0aab334d5a6b141e6d007e1bc6cc518fe15
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-02-23 11:49:54 +0000

    Added API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -24,7 +24,7 @@ JOBS = [
     'id': 4,
     'title': 'Data Engineer',
     'location': 'Absterdam, Netherlands',
-    'salary': ''
+    'salary': '250,000'
   },
 ]
 
@@ -33,6 +33,9 @@ JOBS = [
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name="Turin")
 
-
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
